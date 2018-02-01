5b6a37848b13fcc2c54006e154415645d55ac9b1
Elena Burlacu <elena.burlacu@ndcn.ox.ac.uk>
2018-02-01 15:13:05 +0000

    script with stdout and log file

--- bam2bed.py
+++ bam2bed.py
@@ -7,15 +7,29 @@ Created on Thu Feb  1 11:21:48 2018
 """
 
 import pysam
+import sys
+import logging as L
 
+L.basicConfig(filename = "MyLog.log", level = L.DEBUG)
 fragment_end = 0
+pairs_count = 0
+average_fragmentssize = 0
+sum_intervals = 0
 
-bamfile = pysam.AlignmentFile('../BEL033_1000.bam', 'rb')
-output = open('BEL033_bam2bed.bed', 'w')
+bamfile = pysam.AlignmentFile("BEL033_1000.bam", 'rb')
 iter = bamfile.fetch() # read over the entire file - each line will be an AlignmentSegment class
+initial_count = bamfile.count()
 for aln in iter :
     if aln.is_paired and aln.is_read1:
+        pairs_count += 1
         fragment_end = aln.reference_start + aln.template_length
-        output.write(aln.reference_name + "\t"+ str(aln.reference_start) + "\t"+ str(fragment_end) +"\t"+aln.query_name + "." +"\t"+"," + "\n")
+        sum_intervals += aln.template_length
+        sys.stdout.write(aln.reference_name + "\t"+ str(aln.reference_start) + "\t"+ str(fragment_end) +"\t"+aln.query_name + "." +"\t"+"," + "\n")
+        
+average_fragmentssize = sum_intervals / pairs_count
+L.info("Initial count is {}".format(initial_count))
+L.info("Number of paired reads is {}".format(pairs_count))
+L.info("Average fragment size is {:4.2f}".format(average_fragmentssize))
+
+
 bamfile.close()
-output.close()
